4f64443e07ad78f349cc13a61028d5ef4426d0ff
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-08-28 20:49:27 +0530

    udpated egnitely client

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use aws_sdk_dynamodb::{model::AttributeValue, types::DateTime, Client};
-use egnitely_client::{RequestContext, Result};
+use egnitely_client::{Context, Error};
 use serde::{Deserialize, Serialize};
 use serde_dynamo::aws_sdk_dynamodb_0_17::to_item;
 use serde_json::{json, Value};
-use std::{time::SystemTime};
+use std::time::SystemTime;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -11,32 +11,32 @@ struct FunctionContextData {
     pub table_name: String,
 }
 
-pub async fn handler(mut _ctx: RequestContext, _input: Value) -> Result<Value> {
+pub async fn handler(mut _ctx: Context, _input: Value) -> Result<Value, Error> {
     let mut item_data = to_item(_input)?;
     let function_id = Uuid::new_v4().to_string();
-    if let Some(sdk_config) = _ctx.aws_sdk_config() {
-        let client = Client::new(&sdk_config);
+    let config = aws_config::from_env().region("ap-south-1").load().await;
 
-        item_data.insert("id".to_string(), AttributeValue::S(function_id.clone()));
-        item_data.insert(
-            "created_at".to_string(),
-            AttributeValue::N(DateTime::from(SystemTime::now()).as_nanos().to_string()),
-        );
-        item_data.insert(
-            "updated_at".to_string(),
-            AttributeValue::N(DateTime::from(SystemTime::now()).as_nanos().to_string()),
-        );
-        item_data.insert("deleted_at".to_string(), AttributeValue::Null(true));
+    let client = Client::new(&config);
 
-        let context_data = serde_json::from_value::<FunctionContextData>(_ctx.data())?;
+    item_data.insert("id".to_string(), AttributeValue::S(function_id.clone()));
+    item_data.insert(
+        "created_at".to_string(),
+        AttributeValue::N(DateTime::from(SystemTime::now()).as_nanos().to_string()),
+    );
+    item_data.insert(
+        "updated_at".to_string(),
+        AttributeValue::N(DateTime::from(SystemTime::now()).as_nanos().to_string()),
+    );
+    item_data.insert("deleted_at".to_string(), AttributeValue::Null(true));
 
-        let request = client
-            .put_item()
-            .table_name(context_data.table_name)
-            .set_item(Some(item_data));
+    let context_data = serde_json::from_value::<FunctionContextData>(_ctx.config())?;
 
-        request.send().await?;
-    }
+    let request = client
+        .put_item()
+        .table_name(context_data.table_name)
+        .set_item(Some(item_data));
+
+    request.send().await?;
     Ok(json!({
         "message": "function created successfully",
         "data": {
@@ -48,27 +48,14 @@ pub async fn handler(mut _ctx: RequestContext, _input: Value) -> Result<Value> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use aws_sdk_dynamodb::Credentials;
 
     #[tokio::test]
     async fn trigger_function() {
-        let config = aws_config::from_env()
-            .credentials_provider(Credentials::new(
-                "PUT_ACCESS_KEY_HERE".to_string(),
-                "PUT_ACCESS_SECRET_HERE".to_string(),
-                None,
-                None,
-                "local",
-            ))
-            .region("ap-south-1")
-            .load()
-            .await;
 
         let resp = handler(
-            RequestContext::new(
+            Context::new(
                 "test".to_string(),
                 "test".to_string(),
-                Some(config),
                 json!({
                     "table_name": "functions"
                 }),
